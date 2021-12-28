3d2fd9342cfb98ca5c63c39c63a7415d0f2880a2
zfoteff <zfoteff@zagmail.gonzaga.edu>
2021-12-27 20:06:35 -0800

    Home page mostly set up. Setting up repo for proper version control

---
+++ routes/health.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET health of site */
+
+router.get('/', function(req, res, next) {
+    health = {
+        'header': req.headers,
+        'uptime': process.uptime(),
+        'date': Date.now(),
+        'message': 'Thanks for visiting my site!'
+    }
+    res.json(health);
+});
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+const intro_string = "\tHello! My name is Zac Foteff and I am a student at Gonzaga University pursing a Bachelor of Science focused in Computer Science with a minor in Mathematics. " +
+    "I am interested in pursing careers as a backend software developer, or in the computer/network security field.\n\n\tMy Journey with computer science began when I was a freshman " +
+    "in highschool. I discovered a Python IDE for iPad called Pythonista during Christmas vacation and experimented with every module I could -- even though I knew nothing about computer " +
+    "syntax, or what a programming language even was at the time. Nevertheless, by the end of the break I was enamoured with the subject of computer science and was certain that I had " +
+    "found something that I wanted to do for the rest of my life. Folowing that discovery, I took every computer science class that my highchool offered and continued to expand my knowledge at Gonzaga.";
+
+const skills_string = "\tMy learning isn't just limited to the classroom. I spend much of my free time researching and " +
+    "designing projects for myself in order to challenge myself and gain practical knowledge of the many different " +
+    "aspects of computer science. You can find a selection of these projects displayed in the Personal Projects " +
+    "tab of this website. In addition to various personal projects, I have devoted lots of time to learning " +
+    "various other skills need in computer science and software development. I have a good understanding of Git " +
+    "and its various tools for managing workflow, as well as developing a good understanding of the Linux" +
+    "operating system.Additionally, I have devoted lots of time towards learning to use and developing with " +
+    "Arduinos and Raspberry Pis.I especially love developing projects with the Arduino to bridge the gap " +
+    "between hardware and software.";
+
+
+const personal_string = "\tIn my personal life, I am a big brother with two younger sisters. I " +
+    "am an avid Lacrosse player and a passionate Oregon Ducks football fan. I enjoy spending lots of time outdoors " +
+    " skiing and hiking in my home state of Oregon In the future, I'm looking forward to having the opportunity to " +
+    "coach a youth Lacrosse team and to travel to all 50 states in the United States";
+
+
+/* GET home page. */
+data = {
+    intro: intro_string,
+    skills: skills_string,
+    personal: personal_string
+};
+
+router.get('/', function(req, res, next) {
+    res.render('home', data);
+});
+
+module.exports = router;
